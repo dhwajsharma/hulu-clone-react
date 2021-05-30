67ce53a2137d4af47ce34a56673e882fc87b0f24
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-05-31 02:46:33 +0530

    Building VideoCard

---
+++ src/components/Results/Results.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import VideoCard from '../VideoCard/VideoCard'
+import "./Results.css"
+
+function Results() {
+    return (
+        <div className="results">
+            <VideoCard />
+        </div>
+    )
+}
+
+export default Results
