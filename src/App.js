fd5cd4b8331be45a4ad9843c85bdb68f89624460
Dhwaj Sharma <dhwajsharma10@gmail.com>
2021-05-31 02:24:50 +0530

    Built Header.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="app">
-      <h2>hi</h2>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,39 @@
+import { FlashOn, Home, LiveTv, PersonOutline, Search, VideoLibrary } from '@material-ui/icons'
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__icons">
+                <div className="header__icon header__icon--active">
+                    <Home />
+                    <p>Home</p>
+                </div>
+                <div className="header__icon">
+                    <FlashOn />
+                    <p>Trending</p>
+                </div>
+                <div className="header__icon">
+                    <LiveTv />
+                    <p>Verified</p>
+                </div>
+                <div className="header__icon">
+                    <VideoLibrary />
+                    <p>Collections</p>
+                </div>
+                <div className="header__icon">
+                    <Search />
+                    <p>Search</p>
+                </div>
+                <div className="header__icon">
+                    <PersonOutline />
+                    <p>Account</p>
+                </div>
+            </div>
+            <img src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png" alt="hulu" />
+        </div>
+    )
+}
+
+export default Header
